d676007612f02e884e82e8f3e6b73b0b53ebbbb5
Jayanth-T <106177371+Jayanth-T@users.noreply.github.com>
2024-04-01 14:19:02 +0530

    Update Exp1_Fulladder.v

--- Exp1_Fulladder.v
+++ Exp1_Fulladder.v
@@ -1,12 +1,11 @@
-module full_adder(
-    input a,
-    input b,
-    input cin,
-    output sum,
-    output cout
-);
-
-assign sum = a ^ b ^ cin;
-assign cout = (a & b) | (cin & (a ^ b));
-
+module fulladder (sum, cout, a,b,c);
+input a,b,c;
+output sum, cout;
+wire w1,w2,w3,w4,w5;
+xor x1(w1,a,b);
+xor x2(sum,w1,c);
+and al(w2,a,b);
+and a2(w3,b,c);
+and a3(w4,a,c);
+or o1(w5,w2,w3); or o2(cout,w5,w4);
 endmodule
